ecc0e3f6295bea48c8e2845d491e0fe1ffd0de19
hasbiradifan123 <hasbiradifanl123@gmail.com>
2024-11-25 23:00:00 +0700

    update revisi feature Portfolio Title

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -150,7 +150,7 @@ const projects = [
 const Portfolio = () => {
   return (
     <div className="max-w-[1000px] mx-auto p-6 md:my-20" id="portfolio">
-      <h2 className="text-3xl font-bold text-center text-gray-400 mb-8">
+      <h2 className="text-3xl font-bold text-center text-gray-200 mb-8">
         Portfolio
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
